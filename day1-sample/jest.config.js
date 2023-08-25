93eb01df07488e8b917dadd9767cdc6534b7a628
leoreyesdev <colombia38@hotmail.com>
2023-08-25 13:54:41 -0600

    install jest

--- day1-sample/jest.config.js
+++ day1-sample/jest.config.js
@@ -1,3 +1,15 @@
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest'
-}
+  moduleFileExtensions: ['js', 'json', 'vue'],
+  transform: {
+    '^.+\\.vue$': 'vue-jest',
+    '^.+\\.js$': 'babel-jest'
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  },
+  testMatch: [
+    '**/tests/unit/**/*.spec.[jt]s?(x)',
+    '**/__tests__/*.[jt]s?(x)'
+  ],
+  transformIgnorePatterns: ['/node_modules/']
+};
--- day1-sample/tests/unit/ItemsComponent.spec.js
+++ day1-sample/tests/unit/ItemsComponent.spec.js
@@ -1,53 +1,104 @@
-import { mount } from "@vue/test-utils";
-import ItemsComponent from "../../src/components/ItemsFromMongo.vue";  // Replace with the actual path to your component
+// import { mount } from "@vue/test-utils";
+// import ItemsComponent from "../../src/components/ItemsFromMongo.vue";  // Replace with the actual path to your component
 
-const mockJsonPromise = (data) => Promise.resolve(data);
-const mockFetchPromise = (response) => Promise.resolve({
-    json: () => mockJsonPromise(response),
-});
+// const mockJsonPromise = (data) => Promise.resolve(data);
+// const mockFetchPromise = (response) => Promise.resolve({
+//     json: () => mockJsonPromise(response),
+// });
 
-global.Response = mockFetchPromise;
+// describe("ItemsComponent", () => {
+//     let mockFetch;
 
+//     beforeEach(() => {
+//         mockFetch = jest.fn();
+//         global.fetch = mockFetch;
+//     });
 
-describe("ItemsComponent", () => {
-    let mockFetch = jest.fn();
+//     it("fetches items on created lifecycle hook", async () => {
+//         mockFetch.mockResolvedValueOnce(mockFetchPromise([
+//             { _id: "1", name: "Test Item 1" },
+//             { _id: "2", name: "Test Item 2" }
+//         ]));
 
-    // This will replace the global fetch with a mock function for testing
-    beforeEach(() => {
-        global.fetch = mockFetch;
+//         const wrapper = mount(ItemsComponent);
+//         await wrapper.vm.$nextTick();
+
+//         expect(mockFetch).toHaveBeenCalled();
+//         expect(wrapper.vm.items).toEqual([
+//             { _id: "1", name: "Test Item 1" },
+//             { _id: "2", name: "Test Item 2" }
+//         ]);
+//     });
+
+//     it("adds an item when 'Add Item' button is clicked", async () => {
+//         const newItem = { _id: "3", name: "Test Item 3" };
+//         mockFetch.mockResolvedValueOnce(mockFetchPromise(newItem));
+
+//         const wrapper = mount(ItemsComponent);
+
+//         // Simulate adding new item
+//         await wrapper.setData({ newItem: "Test Item 3" });
+//         await wrapper.find("button:contains('Add Item')").trigger("click");
+//         await wrapper.vm.$nextTick();
+
+//         expect(mockFetch).toHaveBeenCalled();
+//         expect(mockFetch.mock.calls[0][0]).toBe("http://localhost:3000/items");
+//         console.log(wrapper.vm.items);
+//         expect(wrapper.vm.items).toContainEqual(newItem);
+//     });
+
+//     // Additional tests for edit and delete functionality can be added similarly.
+// });
+// import { shallowMount } from '@vue/test-utils'
+// import ItemsComponent from "../../src/components/ItemsFromMongo.vue";
+
+// // Mock the global fetch function
+// global.fetch = jest.fn(() => Promise.resolve({
+//     json: jest.fn(() => Promise.resolve([{ _id: '123', name: 'Test Item' }])),
+//     ok: true,
+// }));
+
+// describe('ItemsComponent', () => {
+
+//     afterEach(() => {
+//         jest.clearAllMocks();
+//     });
+
+//     it('loads items on creation', async () => {
+//         const wrapper = shallowMount(ItemsComponent);
+//         await wrapper.vm.$nextTick(); // Wait for created() to resolve
+//         expect(wrapper.vm.items).toEqual([{ _id: '123', name: 'Test Item' }]);
+//     });
+
+//     it('adds an item', async () => {
+//         const wrapper = shallowMount(ItemsComponent);
+
+//         wrapper.setData({ newItem: 'Another Test Item' });
+//         await wrapper.vm.addItem();
+
+//         expect(fetch).toHaveBeenCalledWith('http://localhost:3000/items', expect.anything());
+//         expect(wrapper.vm.items).toHaveLength(2);
+//     });
+
+//     it('deletes an item', async () => {
+//         const wrapper = shallowMount(ItemsComponent);
+
+//         await wrapper.vm.deleteItem('123');
+//         expect(fetch).toHaveBeenCalledWith('http://localhost:3000/items/123', expect.anything());
+//         expect(wrapper.vm.items).toHaveLength(0);
+//     });
+
+//     // ... additional tests ...
+
+// });
+
+import { shallowMount } from '@vue/test-utils';
+import ItemsFromMongo from '../../src/components/ItemsFromMongo';
+
+describe('ItemsFromMongo', () => {
+    it('does something', () => {
+        const wrapper = shallowMount(ItemsFromMongo);
+        // ..
+        cosnole.log('WRAPPER', wrapper)
     });
-
-    it("fetches items on created lifecycle hook", async () => {
-        mockFetch.mockResolvedValueOnce(mockFetchPromise([
-            { _id: "1", name: "Test Item 1" },
-            { _id: "2", name: "Test Item 2" }
-        ]));
-
-
-        const wrapper = mount(ItemsComponent);
-        await wrapper.vm.$nextTick();
-
-        expect(wrapper.vm.items).toEqual([
-            { _id: "1", name: "Test Item 1" },
-            { _id: "2", name: "Test Item 2" }
-        ]);
-    });
-
-    it("adds an item when 'Add Item' button is clicked", async () => {
-        const newItem = { _id: "3", name: "Test Item 3" };
-        //mockFetch.mockResolvedValueOnce(new Response(JSON.stringify(newItem)));
-        mockFetch.mockResolvedValueOnce(mockFetchPromise([newItem]));
-
-        const wrapper = mount(ItemsComponent);
-
-        // Simulate adding new item
-        await wrapper.setData({ newItem: "Test Item 3" });
-        await wrapper.find("button").trigger("click");
-        await wrapper.vm.$nextTick();
-
-        expect(wrapper.vm.items).toContainEqual(newItem);
-    });
-
-
-    // Similarly, other tests can be added for edit and delete functionality
 });
